35efb5de70e2d2a9267a6064d62fc16bb0ef4c4f
Andrey <andrey23arce@gmail.com>
2024-11-22 13:36:20 -0700

    CREAR TAG
    Ya funciona añadir una nueva etiqueta

--- views/categories/tags/createTagModal.php
+++ views/categories/tags/createTagModal.php
@@ -5,7 +5,11 @@
                 <h5 class="modal-title" id="addTagModalLabel">Añadir Etiqueta</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"></button>
             </div>
-            <form action="<?= BASE_PATH ?>tags/create/" method="POST">
+            <form action="<?= BASE_PATH ?>categorias/etiquetas" method="POST">
+                <input type="hidden" name="action" value="createTag">
+                <input type="hidden" name="redirect_url" value="categorias/etiquetas">
+                <input type="hidden" name="token" value="<?php echo  $token; ?>">
+                <input type="hidden" name="created_by" value="<?php echo  $created_by; ?>">
                 <div class="modal-body">
                     <div class="mb-3">
                         <label for="name" class="form-label">Nombre</label>
